4a539e946e1a176b64a9601085a50363053cf83a
Your Name <you@example.com>
2025-09-05 23:37:25 +0700

    fix(layout): remove duplicate headers from blog pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,4 @@
 import { Card } from "@/components/Card";
-import { Header } from "@/components/Header";
 import { posts } from "@/lib/posts";
 import { JetBrains_Mono } from "next/font/google";
 
@@ -10,32 +9,29 @@ const jetbrainsMono = JetBrains_Mono({
 
 export default function Blog() {
   return (
-    <div className="flex-1">
-      <Header />
-      <div
-        className={
-          "container flex flex-col items-center gap-16 " +
-          jetbrainsMono.variable
-        }
-      >
-        <div className="mt-16 flex flex-col items-center">
-          <h1 className="text-5xl font-bold">Blog</h1>
-          <p className="mt-4 text-xl">
-            Here you can find all the articles of the blog.
-          </p>
-          </div>
-        <div className="mb-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {posts.map((post) => (
-            <Card
-              key={post.slug}
-              slug={post.slug}
-              title={post.title}
-              author={post.author}
-              date={post.date}
-              excerpt={post.excerpt}
-            />
-          ))}
-          </div>
+    <div
+      className={
+        "container flex flex-col items-center gap-16 " +
+        jetbrainsMono.variable
+      }
+    >
+      <div className="mt-16 flex flex-col items-center">
+        <h1 className="text-5xl font-bold">Blog</h1>
+        <p className="mt-4 text-xl">
+          Here you can find all the articles of the blog.
+        </p>
+        </div>
+      <div className="mb-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Card
+            key={post.slug}
+            slug={post.slug}
+            title={post.title}
+            author={post.author}
+            date={post.date}
+            excerpt={post.excerpt}
+          />
+        ))}
         </div>
       </div>
   );
