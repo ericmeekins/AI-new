fb0bca99ddb0eb1cd0646add1cde30b83cb62bca
Your Name <you@example.com>
2025-09-05 23:19:08 +0700

    chore: remove Faq component for debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import { ActionSection } from "@/components/ActionSection";
 import { Cards } from "@/components/Cards";
 import { Faq } from "@/components/Faq";
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="flex-1">
-      <Header />
+    <>
       <HeroSection />
       <Cards />
       <ActionSection />
-      <Faq />
-      <Footer />
-    </div>
+    </>
   );
 } 
